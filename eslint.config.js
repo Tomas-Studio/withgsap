7bb63222c3a9a1544ddd008b72eaef7909430a39
olakunle.olatunji <tjomas20@gmail.com>
2024-02-20 08:50:03 +0100

    chore: configured eslint

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  rules: {
+    // 'ts/consistent-type-definitions': ['warn', 'type'],
+    'node/prefer-global/process': 'off',
+    'max-statements-per-line': 'off',
+    'style/max-statements-per-line': 'off',
+  },
+})
