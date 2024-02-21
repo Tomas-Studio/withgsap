16b5217b4c641e47b3024b2a371b4684a7ba33cf
olakunle.olatunji <tjomas20@gmail.com>
2024-02-21 01:39:46 +0100

    feat: initial scaffold

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div flex="~ col" min-h-100dvh relative>
+    <WebLayoutNav />
+    <main flex-1>
+      <slot />
+    </main>
+    <WebLayoutFooter />
+  </div>
+</template>
---
+++ uno.config.ts
@@ -0,0 +1,32 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: {},
+  rules: [],
+  theme: {},
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+
+      },
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
+})
