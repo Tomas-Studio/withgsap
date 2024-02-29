03a690190e7e7435c92869dd2238043f5f33d446
olakunle.olatunji <tjomas20@gmail.com>
2024-02-29 17:34:20 +0100

    feat: add vercel insight

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+import { SpeedInsights } from '@vercel/speed-insights/vue'
+</script>
+
 <template>
   <div flex="~ col" min-h-100dvh relative>
     <WebLayoutNav />
@@ -5,5 +9,6 @@
       <slot />
     </main>
     <WebLayoutFooter />
+    <SpeedInsights />
   </div>
 </template>
