3cb212ba2c3b5f6e35b0c37ea87584b580faa9d3
olakunle.olatunji <tjomas20@gmail.com>
2024-03-25 15:17:51 +0100

    chore: touch time count down main

--- components/TimeCountDown/TimeCountDownMain.vue
+++ components/TimeCountDown/TimeCountDownMain.vue
@@ -4,14 +4,18 @@
     <div class="flex-1 fcc">
       <TimeCountDown />
     </div>
-    <footer flex justify-end py5 px4 xl:px10>
-      Made with
-      <NuxtLink to="https://vuejs.org/" target="_blank" external class="mx1 flink hover:text-yellow-7">
-        Vue
-      </NuxtLink>  &
-      <NuxtLink to="https://gsap.com/" target="_blank" external class="mx1 flink hover:text-yellow-7">
-        Gsap
-      </NuxtLink>
+    <footer flex items-center justify-between py5 px4 xl:px10>
+      <div />
+      <div flex items-center>
+        Made with
+        <div i-solar:heart-angle-bold h4 w4 text-g-red mx1 /> using
+        <NuxtLink to="https://vuejs.org/" target="_blank" external class="mx1 flink hover:text-yellow-7">
+          Vue
+        </NuxtLink>  &
+        <NuxtLink to="https://gsap.com/" target="_blank" external class="mx1 flink hover:text-yellow-7">
+          Gsap
+        </NuxtLink>
+      </div>
     </footer>
   </main>
 </template>
