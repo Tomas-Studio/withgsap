8403c5fb4ed7c0b82da17a6c368b4a501f638f41
olakunle.olatunji <tjomas20@gmail.com>
2024-04-01 02:45:25 +0100

    chore: move TimeCountDownMain to count-down page

--- pages/count-down.vue
+++ pages/count-down.vue
@@ -4,5 +4,23 @@ useSeoMeta({ title: 'Time Countdown' })
 </script>
 
 <template>
-  <TimeCountDownMain />
+  <main class="min-h-100dvh wfull flex flex-col bg-gray-2">
+    <ShowInfoDrawerWrapper />
+    <div class="flex-1 fcc">
+      <TimeCountDown />
+    </div>
+    <footer flex items-center justify-between py5 px4 xl:px10>
+      <div />
+      <div flex items-center>
+        Made with
+        <div i-solar:heart-angle-bold h4 w4 text-g-red mx1 /> using
+        <NuxtLink to="https://vuejs.org/" target="_blank" external class="mx1 flink focus-visible:text-yellow-7 hover:text-yellow-7">
+          Vue
+        </NuxtLink>  &
+        <NuxtLink to="https://gsap.com/" target="_blank" external class="mx1 flink focus-visible:text-yellow-7 hover:text-yellow-7">
+          Gsap
+        </NuxtLink>
+      </div>
+    </footer>
+  </main>
 </template>
